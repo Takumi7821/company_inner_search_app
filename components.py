688ed613312a49bf928ca680363cd0bfc3d988e5
takumi7821 <takumi7821@gmail.com>
2026-03-13 01:24:21 +0900

    first commit.

--- components.py
+++ components.py
@@ -68,7 +68,7 @@ def display_right_panel(conv_container=None):
     # 初期メッセージ（会話履歴が空の場合）を会話コンテナに表示
     if not st.session_state.messages:
         with st.chat_message("assistant"):
-            st.success("こんにちは。私は社内文書の情報をもとに回答する生成AIチャットボットです。上記で利用目的を選択し、画面下部のチャット欄からメッセージを送信してください。")
+            st.success("こんにちは。私は社内文書の情報をもとに回答する生成AIチャットボットです。サイドメニューで利用目的を選択し、画面下部のチャット欄からメッセージを送信してください。")
 
     target.warning("具体的に入力した方が行きたい通りの回答を得られやすです。")
 
